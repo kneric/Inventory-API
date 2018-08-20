347c2848ebe4eccae16826151059bb1716c72ffd
kneric <brian.pradipta.a@gmail.com>
2018-08-20 09:38:31 +0700

    added routes

---
+++ server/routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+router
+  .post('/regiser', (req, res) => {
+
+  })
+
+  .post('/request_token', (req, res) => {
+
+  })
+
+  .post('/items', (req, res) => {
+
+  })
+
+  .get('/', (req, res) => {
+    let q = req.query.query
+  })
